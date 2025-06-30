67309e321fe5dbc891de85dcd335049b785405b0
naimkim <naimk5451@gmail.com>
2025-06-30 22:44:50 +0900

    chore: a002 완

--- 251/week1/a002.java
+++ 251/week1/a002.java
@@ -1,5 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class a002 {
     public static void main(String[] args) {
-        System.out.println("Hello, PPS Week1 Problem2!");
+        new a002().run();
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int numRows = sc.nextInt();
+
+        List<List<Integer>> pascalTriangle = generate(numRows);
+        printTriangle(pascalTriangle);
+    }
+
+    // create pascal < numRows
+    private List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                // first || last row is 1
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int val = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+
+    // Print Pascal Triancle result in pascalTriangle form
+    private void printTriangle(List<List<Integer>> pascalTriangle) {
+        for (List<Integer> row : pascalTriangle) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }

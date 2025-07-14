bdf12e50f0634b8b8eac3d1c2d156358666675f4
naimkim <naimk5451@gmail.com>
2025-07-14 21:46:13 +0900

    chore:26-40 setup

---
+++ 251/week2/a037.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/lemonade-change/description/
+public class a024 {
+
+    public static void main(String[] args) {
+        new a024().run();
+    }
+
+    public void run() {
+        int[] bills = { 5, 5, 5, 10, 20 };
+        System.out.println(lemonadeChange(bills)); // Output: true
+    }
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0)
+                    return false;
+                five--;
+                ten++;
+            } else { // bill == 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

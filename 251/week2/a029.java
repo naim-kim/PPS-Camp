4fae5b7d1f1003fe6f15a2ebe9f829c823f37ab9
naimkim <naimk5451@gmail.com>
2025-07-15 17:57:10 +0900

    chore: refractor

--- 251/week2/a029.java
+++ 251/week2/a029.java
@@ -5,18 +5,27 @@ import java.util.Scanner;
 public class a029 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 입력값이 충분히 들어오는지 확인 필요
+        if (!sc.hasNextInt())
+            return;
         int N = sc.nextInt();
+
+        if (!sc.hasNextInt())
+            return;
         int first = sc.nextInt();
+
         sc.close();
 
-        if (N > 5) {
+        // 규칙 상 6번째 문부터는 불가능
+        if (N >= 6) {
             System.out.println("Love is open door");
             return;
         }
 
         int state = first;
         for (int i = 1; i < N; i++) {
-            state ^= 1;
+            state ^= 1; // toggle
             System.out.println(state);
         }
     }
